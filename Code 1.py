d6d30cb25b109d0b398036d268fc6b57444206f1
Osborn Osayande <O.Osayande16@KCC.local>
2020-09-16 13:11:00 +0100

    Signed-off-by: Osborn Osayande <O.Osayande16@KCC.local> string method

--- Code 1.py
+++ Code 1.py
@@ -1,1 +1,17 @@
-print("Github")
+#firstname = "Osborn"
+# surname = "Osayande"
+# year = str(16)
+# 
+# 
+# email = firstname+surname+year+"@kishogecc.ie"
+# 
+# print(email)
+
+firstname = input("what is your firstname")
+surname =input("what is your surname")
+year = str(16)
+
+
+email = firstname+surname+year+"@kishogecc.ie"
+
+print(email)
---
+++ String methods.py
@@ -0,0 +1,12 @@
+
+un = input("please type your username")
+pw = input("please type your password")
+
+print("#"*20)
+
+print("Your username is" , un.lower(),"#") 
+print("#","Your password is" ,pw.capitalize(),"#")
+length = len(pw)
+print ("#","your password is",length,"characters long","#")
+
+print("#"*20)
